18e439998635ac250b75052f823ae61db5171d71
tassiio <nastik124@gmail.com>
2021-10-25 11:32:15 +0300

    testing ignore

---
+++ Class_LIFO/cmake-build-debug/CMakeFiles/Class_LIFO.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/1/Mephi/C++/projects/taski/OOP/Class_LIFO/Class_LIFO.cpp" "C:/1/Mephi/C++/projects/taski/OOP/Class_LIFO/cmake-build-debug/CMakeFiles/Class_LIFO.dir/Class_LIFO.cpp.obj"
+  "C:/1/Mephi/C++/projects/taski/OOP/Class_LIFO/main.cpp" "C:/1/Mephi/C++/projects/taski/OOP/Class_LIFO/cmake-build-debug/CMakeFiles/Class_LIFO.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Class_LIFO/cmake-build-debug/CMakeFiles/Class_LIFO.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Class_LIFO.dir/Class_LIFO.cpp.obj"
+  "CMakeFiles/Class_LIFO.dir/main.cpp.obj"
+  "Class_LIFO.exe"
+  "Class_LIFO.exe.manifest"
+  "Class_LIFO.pdb"
+  "libClass_LIFO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Class_LIFO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
